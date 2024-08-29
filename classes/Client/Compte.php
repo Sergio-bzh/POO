e19b2a31e8d923ad13bcc4e68b47f1f5119abe55
sergio-nunez <sergio.nunez@laposte.net>
2024-08-29 22:32:50 +0200

    Ajout d'un autoloader pour tirer profit du méchanisme des namespaces

--- classes/Client/Compte.php
+++ classes/Client/Compte.php
@@ -4,5 +4,35 @@ namespace App\Client;
 
 class Compte
 {
+    private $nom;
+    private $prenom;
+
+    // public function __construct(string $nom, string $prenom)
+    // {
+    //     $this->nom = $nom;
+    //     $this->prenom = $prenom;
+    // }
+
+    // public function getNom():string
+    // {
+    //     return $this->nom;
+    // }
+
+    // public function setNom(string $nom):self
+    // {
+    //     $this->nom = $nom;
+    //     return $this;
+    // }
+
+    // public function getPrenom():string
+    // {
+    //     return $this->prenom;
+    // }
+
+    // public function setPrenom(string $prenom):self
+    // {
+    //     $this->prenom = $prenom;
+    //     return $this;
+    // }
 
 }
